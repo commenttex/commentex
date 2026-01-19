1e94915dd209c46d89d2996722bd6ab5ca705f68
jnsthepigeon <kuepperjonas@icloud.com>
2026-01-19 17:04:40 +0100

    feat: add codemirror with latex lang

--- packages/commentex-frontend/src/App.tsx
+++ packages/commentex-frontend/src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+
+import ReactCodeMirror from '@uiw/react-codemirror'
 import './App.css'
+import { latex } from 'codemirror-lang-latex'
 
 function App() {
   return (
     <>
-      
+      <ReactCodeMirror extensions={[latex()]}></ReactCodeMirror>
     </>
   )
 }
