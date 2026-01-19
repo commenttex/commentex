4703ef94ad2395d9ebf1e2a97ab45cf3869a3e78
jnsthepigeon <kuepperjonas@icloud.com>
2026-01-19 17:56:22 +0100

    feat: add basic styling

--- packages/commentex-frontend/src/App.tsx
+++ packages/commentex-frontend/src/App.tsx
@@ -1,14 +1,17 @@
-
-import ReactCodeMirror from '@uiw/react-codemirror'
-import './App.css'
-import { latex } from 'codemirror-lang-latex'
+import ReactCodeMirror, { EditorView } from "@uiw/react-codemirror";
+import "./App.css";
+import { latex } from "codemirror-lang-latex";
 
 function App() {
   return (
-    <>
-      <ReactCodeMirror extensions={[latex()]}></ReactCodeMirror>
-    </>
-  )
+    <div className="flex justify-center">
+      
+      <ReactCodeMirror
+        className="min-w-3/4 max-w-3/4"
+        extensions={[latex(), EditorView.lineWrapping]}
+      ></ReactCodeMirror>
+    </div>
+  );
 }
 
-export default App
+export default App;
